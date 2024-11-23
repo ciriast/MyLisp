73f496e099ffd805af5bda78ed507dc287fea8b6
ciriast <ciriastorres@gmail.com>
2024-11-23 11:18:16 -0600

    feat: only changes the end of the main function

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
     printf("Hey, you, I'm building my own lisp in C!");
     
-    return 0;
+    // I learned that the EXIT_SUCCESS statement is declared in stdlib
+    return EXIT_SUCCESS;
 }
