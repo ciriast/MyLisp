a29f57253ee8d1bb84a474b6ef8c44593f819d6c
ciriast <ciriastorres@gmail.com>
2024-11-23 11:58:30 -0600

    feat: Add the main while

--- main.c
+++ main.c
@@ -4,8 +4,17 @@
 static char userInput[2048];
 
 int main(int argc, char** argv) {
-    printf("Hey, you, I'm building my own lisp in C!");
+    puts("Lisp version 0.1");
+    puts("Press Ctrl+c to Exit");
     
+    while(1) {
+        fputs("lispy> ", stdout);
+
+        fgets(userInput, 2048, stdin);
+
+        printf("No you are a %s", userInput);
+
+    }
     // I learned that the EXIT_SUCCESS statement is declared in stdlib
     return EXIT_SUCCESS;
 }
