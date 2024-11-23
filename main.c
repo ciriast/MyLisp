855b3c8849cc9af63e70bec8229c7c7767291773
ciriast <ciriastorres@gmail.com>
2024-11-23 15:41:10 -0600

    Add validation to check if the project is runnign in windows

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/* If we are compiling on windows compile these functions */
+#ifdef _WIN32 
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+
+    char* cpy = malloc(strlen(buffer) + 1);
+
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwhise include the editline headers */
+#else
 #include <editline/readline.h>
 #include <editline/history.h>
+#endif
 
 int main(int argc, char** argv) {
     puts("Lisp version 0.1");
